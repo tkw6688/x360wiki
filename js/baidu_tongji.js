6edfa495b4207c2a9814437b45ef62e7b0e62d1f
tkw6688 <tkw666888@outlook.com>
2024-09-22 10:33:56 +0800

    Deployed  with MkDocs version: 1.6.1

---
+++ js/baidu_tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?f88af00151ce62ca86f693b50747fd66";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
